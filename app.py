95c194c9f08acf7a5d8adf39b5e46c579bbdb70c
youssef-e <youssef.ellaabi@everycheck.fr>
2020-08-10 12:05:03 +0200

    "fixed image not canging when reuploading another"

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import sys
 import cv2
 import json
 import socket
+import random
 src_path = os.path.join(".", "src")
 sys.path.append(src_path)
 from ocr import (run, get_parent_dir)
@@ -18,7 +19,7 @@ app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 detection_results_folder = os.path.join(get_parent_dir(n=1), "results")
 detection_results_file = os.path.join(detection_results_folder, "Detection_Results.json")
 json_file = os.path.join(detection_results_folder ,"Detection_Result.json")
-
+current_image_number=0;
 @app.route("/")
 def index():
   return render_template("index.html")
@@ -29,20 +30,30 @@ def about():
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
+   if request.method == 'POST':    
+      try:
+         imagename = "{}.png".format(current_image_number)
+         ofilename = os.path.join(app.config['UPLOAD_FOLDER'],imagename)
+         os.remove(ofilename)
+      except (FileNotFoundError, UnboundLocalError):
+         print('no file to remove')
 
+      f = request.files['file']
       # create a secure filename
       filename = secure_filename(f.filename)
       # save file to /static/uploads
       filepath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
-      f.save(filepath)
+      try:
+         f.save(filepath)
+      except IsADirectoryError:
+         return render_template("index.html")
       
       # load the example image and convert it to grayscale
       gray = run(filepath)
 
       # save the processed image in the /static/uploads directory
-      imagename = "{}.png".format(os.getpid())
+      current_image_number = random.randrange(1000,9999)
+      imagename = "{}.png".format(current_image_number)
       ofilename = os.path.join(app.config['UPLOAD_FOLDER'],imagename)
       cv2.imwrite(ofilename, gray)
       print("ofilename::",ofilename)
@@ -56,13 +67,12 @@ def upload_file():
       for c in data:
          rslt += c +" : "+ data[c] + "\n"
 
-      # os.remove(ofilename)
+      
       os.remove(filepath)
       return render_template("uploaded.html", displaytext= rslt, fname=imagename)
-      # remove the processed image
-
+      
    if request.method == 'GET':
-      imagename = "{}.png".format(os.getpid())
+      imagename = "{}.png".format(current_image_number)
       ofilename = os.path.join(app.config['UPLOAD_FOLDER'],imagename)
       rslt = ""
       # perform OCR on the processed image
